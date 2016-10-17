eb038c38adcf64437457c59f4dd106f8f834f1ca
Nazar Kulyk <nazar.kulyk@ibsolution.de>
2016-10-17 23:44:04 +0200

    add link

--- js/app.js
+++ js/app.js
@@ -1,34 +1,29 @@
 angular.module('BookTableApp', ['ngMaterial', 'angular-loading-bar', 'ngAnimate', 'ngAria', 'ngResource']);
 
 angular.module('BookTableApp')
-  .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
-    cfpLoadingBarProvider.includeSpinner = false;
-  }]);
+    .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+        cfpLoadingBarProvider.includeSpinner = false;
+    }]);
 
 angular.module('BookTableApp')
     .controller('TestCtrl', ['$scope', '$resource', function($scope, $resource) {
         $scope.today = new Date();
 
         var menus = $resource('data/menu.json', null, {
-          query: {
-            method: 'GET',
-            isArray: true
-          }
+            query: {
+                method: 'GET',
+                isArray: true
+            }
         }).query().$promise;
 
         menus.then(function(data) {
-          $scope.menus = data;
+            $scope.menus = data;
         });
 
-/*
-        _.times(4, function(index) {
-            todos.push({
-                //face: imagePath,
-                what: "Brunch this weekend?",
-                who: "Min Li Chan",
-                notes: "I'll be in your neighborhood doing errands."
-            });
-        });
-        $scope.todos = todos;
-        */
+        $scope.redirectToLink = function(link) {
+            if(!link) {
+              return false;
+            }
+            $window.open(link, '_blank');
+        };
     }]);
