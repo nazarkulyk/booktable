d8927c3c29c65e9c2d24f3880e49aecc962adb96
Nazar Kulyk <nazar.kulyk@ibsolution.de>
2016-10-17 20:55:21 +0200

    add title

--- js/app.js
+++ js/app.js
@@ -3,4 +3,5 @@ angular.module('BookTableApp', ['ngMaterial']);
 angular.module('BookTableApp')
 .controller('TestCtrl', ['$scope', function($scope) {
   $scope.test = 'Test';
+  $scope.title = 'My App Title';
 }]);
