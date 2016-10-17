534ec5ab16981550035bc6e34259031e2f0c38e0
Nazar Kulyk <nazar.kulyk@ibsolution.de>
2016-10-17 21:05:44 +0200

    add lodash

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@ angular.module('BookTableApp', ['ngMaterial']);
 
 angular.module('BookTableApp')
 .controller('TestCtrl', ['$scope', function($scope) {
-  $scope.test = 'Test';
+  var todos = [];
+  $scope.test = 'Test ' + _.VERSION;
   $scope.title = 'My App Title';
+
+  for (var i = 0; i < 15; i++) {
+    todos.push({
+      //face: imagePath,
+      what: "Brunch this weekend?",
+      who: "Min Li Chan",
+      notes: "I'll be in your neighborhood doing errands."
+    });
+  }
+  $scope.todos = todos;
 }]);
